f1a43f029927c5a8cfba155348a54bc7d9470d72
apostolicfire ndaa <gadisfx96@gmail.com>
2026-04-16 14:06:06 +0000

    marts now heading to looker

--- models/marts/mart_weather_trend_analysis.sql
+++ models/marts/mart_weather_trend_analysis.sql
@@ -1,4 +1,29 @@
 {{ config(materialized='table') }}
 
-select wind_speed_10m,temperature_2m,time
-from {{ ref('hourly_stg') }}
+WITH weather AS (
+  SELECT *
+  FROM {{ ref('hourly_stg') }}
+),
+
+agg AS (
+  SELECT
+    time,
+    AVG(wind_speed_10m) AS wind_speed,
+    AVG(temperature_2m) AS temperature
+  FROM weather
+  GROUP BY time
+)
+
+SELECT 
+  time,
+  wind_speed,
+  temperature,
+
+  CASE
+  WHEN temperature < 7 THEN 'Very Cold Day'
+  WHEN temperature < 12 THEN 'Cold Day'
+  WHEN temperature < 16 THEN 'Normal Day'
+  ELSE 'Hot Day'
+END AS temp_buckets
+
+FROM agg
